63efff0dff7740ba08f2fd58ac5439f80595c1b0
Supragya2912 <anandsupragya@gmail.com>
2024-07-12 13:50:02 +0530

    add frontend

---
+++ todo-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo app</h1>
+      <h2> This is todo</h2>
+    </div>
+  );
+}
+
+export default App;
